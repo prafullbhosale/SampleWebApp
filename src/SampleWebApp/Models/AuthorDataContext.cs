40ba863228a31997f875af5e59e9fa162a5d15a8
prafullbhosale <prbhosal@microsoft.com>
2016-03-31 16:25:50 -0700

    ApiControllersWithContext

---
+++ src/SampleWebApp/Models/AuthorDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleWebApp.Models;
+
+namespace SampleWebApp.Data
+{
+    public class AuthorDataContext : DbContext
+    {
+        public DbSet<Author> Author { get; set; }
+
+        public AuthorDataContext (DbContextOptions<AuthorDataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
