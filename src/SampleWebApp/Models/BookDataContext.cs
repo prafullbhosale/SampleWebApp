55d7bfe144d2fb399a92294ae056352c8cc61a79
prafullbhosale <prbhosal@microsoft.com>
2016-03-31 16:31:00 -0700

    MvcControllersWithContext

---
+++ src/SampleWebApp/Models/BookDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleWebApp.Models;
+
+namespace SampleWebApp.Data
+{
+    public class BookDataContext : DbContext
+    {
+        public DbSet<Book> Book { get; set; }
+
+        public BookDataContext (DbContextOptions<BookDataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
